31d80a01a6ef954b4bbf936cb9f99a01a7436ed9
Jim Fleming <jim@freeflow.io>
2011-06-10 23:08:56 -0400

    using the simpler version for the demo code

--- public/main.js
+++ public/main.js
@@ -1,80 +1,53 @@
 (function() {
-  var normalizeEl, parseDOM, parts, simpleDOM, simpleText, wrapDiv;
-  wrapDiv = function(textNode) {
-    var span;
-    span = document.createElement('div');
-    span.appendChild(document.createTextNode(textNode.nodeValue));
-    return span;
-  };
-  simpleDOM = [];
-  simpleText = [];
-  normalizeEl = function(el) {
-    var childEl;
-    if (!(el != null)) {
-      return;
-    }
-    if (el.nodeType === 3) {
-      simpleDOM.push(wrapDiv(el));
-      simpleText.push(el.nodeValue);
-    } else if (el.nodeName === 'BR') {
-      simpleDOM.push(el);
-      simpleText.push('\n');
-    } else {
-      childEl = el.firstChild;
-      normalizeEl(childEl);
-      if (childEl != null) {
-        while (childEl = childEl.nextSibling) {
-          normalizeEl(childEl);
-        }
-      }
-    }
-  };
-  parseDOM = function(el) {};
-  parts = ['title', 'author', 'body'];
   $(function() {
-    var $content;
-    $content = $('#content');
-    return $content.bind('keyup', function(e) {
-      var el, i, node, part, part_i, tag, tags, text;
-      el = document.getElementById('content');
-      /*
-          range = document.createRange()
-          range.selectNodeContents(el)
-          range.collapse(true)
-          */
-      simpleDOM = [];
-      simpleText = [];
-      normalizeEl(el);
-      tags = document.getElementById('tags');
-      tags.innerHTML = '';
-      part_i = 0;
-      for (i in simpleDOM) {
-        node = simpleDOM[i];
-        text = simpleText[i];
-        tag = document.createElement('li');
-        tags.appendChild(tag);
-        part = parts[part_i];
-        if (!(part != null)) {
-          continue;
-        }
-        if (!/\S+/.test(text)) {
-          continue;
-        }
-        tag.innerHTML = part;
-        tag.className = 'tag';
-        node.className = part;
-        part_i++;
+    var $content, $metadata, parse_content, update_metadata;
+    $content = $('textarea#content');
+    $metadata = $('dl#metadata');
+    parse_content = function(content) {
+      var author, body, date, lines, metadata, possible_date, title;
+      lines = content.split(/\r?\n/g);
+      lines = _.select(lines, function(line) {
+        return line.length > 0;
+      });
+      title = lines.shift();
+      author = lines.shift();
+      possible_date = lines.shift();
+      date = new Date(possible_date);
+      if (date.toString() === 'Invalid Date') {
+        date = null;
       }
-      tag = document.createElement('li');
-      tags.appendChild(tag);
-      part = parts[part_i];
-      console.log(simpleDOM);
-      console.log(simpleText);
-      /*
-          selection = window.getSelection()
-          selection.removeAllRanges()
-          selection.addRange(range)
-          */
+      metadata = {
+        title: title,
+        author: author
+      };
+      if (date != null) {
+        metadata['date'] = date;
+      }
+      if (lines.length || !(date != null) && possible_date) {
+        body = lines.join('<br>');
+        if (!(date != null)) {
+          body = possible_date + '<br>' + body;
+        }
+      }
+      metadata['body'] = body;
+      return metadata;
+    };
+    update_metadata = function(metadata) {
+      $metadata.empty();
+      return _.each(metadata, function(val, key) {
+        if (val != null) {
+          $metadata.append("<dt>" + key + "</dt>");
+          return $metadata.append("<dd>" + val + "</dd>");
+        } else {
+          $metadata.append("<dt class='error'>" + key + "</dt>");
+          return $metadata.append("<dd class='error'>missing</dd>");
+        }
+      });
+    };
+    return $content.bind('keyup', function(e) {
+      var metadata;
+      metadata = parse_content($content.val());
+      return update_metadata(metadata);
     });
   });
 }).call(this);
--- src/main.coffee
+++ src/main.coffee
@@ -1,100 +1,56 @@
-# normalize the DOM tree
-# extract the text from the DOM tree
-# tokenize this text
-# parse the tokens
-
-wrapDiv = (textNode) ->
-  span = document.createElement('div')
-  span.appendChild(document.createTextNode(textNode.nodeValue))
-  return span
-
-simpleDOM = []
-simpleText = []
-
-normalizeEl = (el) ->
-  return if not el?
-
-  if el.nodeType is 3
-    simpleDOM.push wrapDiv(el)
-    simpleText.push el.nodeValue
-  else if el.nodeName is 'BR'
-    simpleDOM.push el
-    simpleText.push '\n'
-  else
-    childEl = el.firstChild
-    normalizeEl(childEl)
-
-    if childEl?
-      while childEl = childEl.nextSibling
-        normalizeEl(childEl)
-
-  return
-
-parseDOM = (el) ->
-  return
-
-parts = [
-  'title'
-  'author'
-  'body'
-]
+# TOKENIZE THE CONTENT
+# SPECIFY WHAT'S WHAT
+# VALIDATE WHAT YOU GET
+# LET USERS KNOW [DISCOVERY]:
+#   WHAT "FIELD" THEY'RE WORKING ON
+#   WHAT "FIELDS" THEY'VE WORKED ON BEFORE
 
 $ ->
 
-  $content = $('#content')
+  $content = $('textarea#content')
+  $metadata = $('dl#metadata')
+
+  parse_content = (content) ->
+    lines = content.split(/\r?\n/g)
+    lines = _.select lines, (line) ->
+      return line.length > 0
+
+    title = lines.shift()
+    author = lines.shift()
+
+    possible_date = lines.shift()
+    date = new Date(possible_date)
+    date = null if date.toString() is 'Invalid Date'
+
+    # validation/preparsing
+
+    metadata =
+      title: title
+      author: author
+
+    metadata['date'] = date if date? # date is optional
+
+    if lines.length or not date? and possible_date
+      body = lines.join('<br>')
+      body = possible_date + '<br>' + body if not date? # if the user didn't specify a date it must be part of the body
+
+    metadata['body'] = body
+
+    return metadata
+
+
+  update_metadata = (metadata) ->
+    $metadata.empty()
+
+    _.each metadata, (val, key) ->
+      if val?
+        $metadata.append("<dt>#{key}</dt>")
+        $metadata.append("<dd>#{val}</dd>")
+      else
+        $metadata.append("<dt class='error'>#{key}</dt>")
+        $metadata.append("<dd class='error'>missing</dd>")
+
 
   $content.bind 'keyup', (e) ->
-    el = document.getElementById('content')
-
-    ###
-    range = document.createRange()
-    range.selectNodeContents(el)
-    range.collapse(true)
-    ###
-
-    simpleDOM = []
-    simpleText = []
-
-    normalizeEl(el)
- 
-    # el.innerHTML = ''
-
-    tags = document.getElementById('tags')
-    tags.innerHTML = ''
-
-    part_i = 0
-
-    for i, node of simpleDOM
-      # el.appendChild(node)
-      text = simpleText[i]
-
-      tag = document.createElement('li')
-      tags.appendChild(tag)
-
-      part = parts[part_i]
-      continue if not part?
-
-      if not /\S+/.test text
-        continue
-
-      tag.innerHTML = part 
-      tag.className = 'tag'
-
-      node.className = part
-
-      part_i++
-
-    tag = document.createElement('li')
-    tags.appendChild(tag)
-    part = parts[part_i]
-
-    console.log simpleDOM
-    console.log simpleText
-
-    ###
-    selection = window.getSelection()
-    selection.removeAllRanges()
-    selection.addRange(range)
-    ###
-
-    return
+    metadata = parse_content($content.val())
+    update_metadata(metadata)
